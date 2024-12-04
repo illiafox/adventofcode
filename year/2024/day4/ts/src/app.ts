ac31947002a481a47d8fb54fd0dba0666a430993
Illia Dymura <illiadimura@gmail.com>
2024-12-05 01:00:45 +0200

    fix: day4

--- year/2024/day4/ts/src/app.ts
+++ year/2024/day4/ts/src/app.ts
@@ -3,57 +3,47 @@ import {PathLike} from "node:fs";
 import fs from "fs/promises";
 import * as os from "node:os";
 
-const word = ["M", "A", "S"];
-
 function traverse(
     arr: string[],
-    symbolIdx: number,
     i: number,
     j: number,
-    nextChange: number[],
+    change: number[],
 ): number {
-    while (true) {
-        i += nextChange[1];
-        j += nextChange[0];
+    const word = ["M", "A", "S"];
 
-        switch (true) {
-            case i < 0 || i >= arr.length:
-            case j < 0 || j >= arr[0].length:
-                return 0;
-        }
+    for (let state = 0; state < word.length; state++) {
+        [i, j] = [i + change[1], j + change[0]];
 
-        if (arr[i][j] != word[symbolIdx]) {
-            return 0;
-        }
-
-        symbolIdx++;
-        if (word.length == symbolIdx) return 1;
+        if (!arr[i] || arr[i][j] !== word[state]) return 0;
     }
+
+    return 1;
 }
 
 function xmas(lines: string[]): [number, number] {
     let part1 = 0;
     let part2 = 0;
 
-    // for part 2
-    const isValidPosition = (i: number, j: number) =>
-        i > 0 && j > 0 && i < lines.length - 1 && j < lines[0].length - 1;
-
     for (let i = 0; i < lines.length; i++) {
-        for (let j = 0; j < lines[0].length; j++) {
-
-            if (lines[i][j] === "X") { // part 1
-                for (const move of [
+        for (let j = 0; j < lines[i].length; j++) {
+            if (lines[i][j] === "X") {
+                // part 1
+                const moves = [
                     [1, 0], [-1, 0], [0, 1], [0, -1], // up, down, right, left
                     [1, 1], [-1, -1], [1, -1], [-1, 1], // diagonals
-                ]) {
-                    part1 += traverse(lines, 0, i, j, move);
+                ]
+
+                for (const move of moves) {
+                    part1 += traverse(lines, i, j, move);
                 }
             }
 
-            if (lines[i][j] === "A") { // part 2
-                if (!isValidPosition(i, j)) continue;
-
+            if (
+                lines[i][j] === "A" &&
+                lines[i - 1]?.[i - 1] && // check bounds
+                lines[i + 1]?.[i + 1]
+            ) {
+                // part 2
                 const firstDiag = lines[i + 1][j + 1] + lines[i - 1][j - 1];
                 const secondDiag = lines[i + 1][j - 1] + lines[i - 1][j + 1];
 
