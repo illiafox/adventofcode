b0f7475412ef3df7214c8e8ebf243b5097a1b4d3
Illia Dymura <illiadimura@gmail.com>
2025-12-04 09:54:10 +0100

    feat: day4 solution

---
+++ year/2025/rust/src/day_4/mod.rs
@@ -0,0 +1,123 @@
+struct Input {
+    map: Vec<Vec<Tile>>,
+}
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
+enum Tile {
+    Empty(),
+    Roll(),
+}
+
+fn read_input(s: &str) -> Vec<Vec<Tile>> {
+    s.lines()
+        .map(|line| {
+            line.chars()
+                .map(|c| match c {
+                    '.' => Tile::Empty(),
+                    '@' => Tile::Roll(),
+                    _ => panic!("wrong input char {c}"),
+                })
+                .collect()
+        })
+        .collect()
+}
+
+fn traverse(map: &[Vec<Tile>]) -> (i64, Vec<(usize, usize)>) {
+    let mut rolls_removed = 0;
+    let mut removed = Vec::new();
+
+    let height = map.len();
+
+    for i in 0..height {
+        let width = map[i].len();
+        for j in 0..width {
+            let elem = map[i][j];
+
+            if let Tile::Empty() = elem {
+                continue;
+            }
+
+            let directions = [
+                (1, 0),
+                (-1, 0),
+                (0, 1),
+                (0, -1),
+                (1, 1),
+                (-1, -1),
+                (-1, 1),
+                (1, -1),
+            ];
+
+            let mut rolls = 0;
+
+            for (dx, dy) in directions {
+                let new_i = i as i32 + dy;
+                let new_j = j as i32 + dx;
+
+                if new_j < 0
+                    || new_j >= map[i].len() as i32
+                    || new_i < 0
+                    || new_i >= map.len() as i32
+                {
+                    continue;
+                }
+
+                let new_i = new_i as usize;
+                let new_j = new_j as usize;
+
+                if map[new_i][new_j] == Tile::Roll() {
+                    rolls += 1;
+                }
+            }
+
+            if rolls < 4 {
+                rolls_removed += 1;
+                removed.push((i, j));
+            }
+        }
+    }
+
+    (rolls_removed, removed)
+}
+
+fn part_one(map: &[Vec<Tile>]) -> i64 {
+    let (rolls_removed, _) = traverse(map);
+
+    rolls_removed
+}
+
+fn part_two(map: &[Vec<Tile>]) -> i64 {
+    let mut cloned: Vec<Vec<Tile>> = map.to_vec();
+
+    let mut out = 0;
+
+    loop {
+        let (removed, points) = traverse(&cloned);
+        out += removed;
+        if removed == 0 {
+            return out;
+        }
+
+        for (i, j) in points {
+            cloned[i][j] = Tile::Empty()
+        }
+    }
+}
+
+#[test]
+fn test_example() {
+    let file = include_str!("example_input.txt");
+    let map = read_input(file);
+
+    assert_eq!(part_one(&map), 13);
+    assert_eq!(part_two(&map), 43);
+}
+
+#[test]
+fn test_custom_input() {
+    let file = include_str!("input.txt");
+    let map = read_input(file);
+
+    assert_eq!(part_one(&map), 1569);
+    assert_eq!(part_two(&map), 9280);
+}
